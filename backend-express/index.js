9bf030e7040a7fe7984236ab47518a33eb3ca588
Ruben Laguna <ruben.laguna@gmail.com>
2022-08-22 17:17:46 +0200

    backend: basic login working

--- backend-express/index.js
+++ backend-express/index.js
@@ -4,15 +4,47 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const db = require('./db');
 const logger = require('./logger');
+const {V4} = require('paseto');
+const crypto = require('crypto');
+const jose = require('jose');
+
 
 require('dotenv').config();
 
-const conn = db();
 
+async function resetDb(conn) {
+  const User = conn.model('User');
+  User.collection.drop();
+  const initialUser = new User({username: 'aaa', password: 'bbb'});
+  const savedDocument = await initialUser.save();
+  logger.info("Created initial user")
+  // logger.info("Initial user %s", savedDocument);
+}
 
 (async () => {
+  const conn = db();
+  const User = conn.model('User');
+  const secretPASERK = process.env.PASETO_SECRET_KEY
+
+  const appId = `http://localhost/thisapp`; // we could use any string or URI
+
+  if (!secretPASERK) {
+    logger.error("PASETO_SECRET_KEY missing from environment variables or .env");
+    process.exit();
+  }
+  const secretKeyBuffer = Buffer.from(secretPASERK.slice(10),'base64url');
+  const secretKey = V4.bytesToKeyObject(secretKeyBuffer); 
+  const publicKey = crypto.createPublicKey(secretKey);
+  const publicJwk = await jose.exportJWK(publicKey); // publicKey is a crypto.KeyObject (PublickKeyObject)
+  publicJwk.kid = await jose.calculateJwkThumbprint(publicJwk, 'sha256');
+  logger.info("Public JWK %s", publicJwk);
+
   const serverPort = 3000;
   const app = express();
+
+  resetDb(conn); // obviously this wouldn't be here in a real application
+
+
 
   app.use(morgan('combined'));
 
@@ -23,13 +55,61 @@ const conn = db();
   };
   app.use(cors(corsOptions));
 
-  app.post('/api/login', (req, res, next) => {
+  // required JSON and X-Requested-By (force CORS in the browser)
+  app.use((req, res, next) => {
+    if (req.is('json') && req.get('x-requested-with')) {
+      return next();
+    } else {
+      res.status(401).json({message: "content-type must be application/json and X-Requested-By header must be present"});
+    }
+  });
+
+  app.post('/api/login', async (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    res.status(401).json({
-      message: "Invalid credentials"
+    const user = await User.findOne({username});
+    if (!user) {
+      logger.info("User %s not found", username)
+      res.status(401).json({
+        message: "Invalid credentials"
+      });
+      return;
+    }
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      logger.info("Password incorrect for User %s", username)
+      res.status(401).json({
+        message: "Invalid credentials"
+      });
+      return;
+    }
+    
+    //TODO generate a PASETO token
+    // https://github.com/panva/paseto/blob/main/docs/README.md#v4signpayload-key-options
+
+    const jti = crypto.randomUUID();
+    const token = await V4.sign({}, secretKey, {
+      sub: user.username,
+      audience: appId,
+      issuer: appId,
+      kid: publicJwk.kid,
+      jti,
+    }) // options is {} 
+
+    res.json({
+      message: "Authentication successful",
+      token,
     });
+  });
+
+  //TODO: reject non JSON requests
+  //TODO: request if X-Requested-By not present
+
+  // Middleware that adds req.username from PASETO token if present in Authentication: Bearer header
+  app.use((req,res,next) => {
+
 
   });
 
