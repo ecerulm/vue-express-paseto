b05d6d97925dd89e8d7904f3f6d870894d087aa7
Ruben Laguna <ruben.laguna@gmail.com>
2022-08-25 16:16:59 +0200

    formatting changes

--- backend-express/db.js
+++ backend-express/db.js
@@ -1,37 +1,40 @@
-const mongoose = require('mongoose');
-const logger = require('./logger');
-const bcrypt = require('bcrypt');
-
+const mongoose = require("mongoose");
+const logger = require("./logger");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
-  username: {type: String, required: true, index: {unique: true}},
-  password: {type: String, required: true},
-})
-
-userSchema.pre('save', function(next) {
-  const user = this;
-  
-  if (!user.isModified("password")) return next(); // only run if password changed
-
-  bcrypt.genSalt().then(salt => {
-    return bcrypt.hash(user.password, salt);
-  }).then(hash => {
-    user.password = hash;
-    return next();
-  }).catch((err) => {
-    return next(err);
-  });
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
+  counter: { type: Number, default: 0 },
 });
 
-userSchema.methods.comparePassword = function(inputPassword) {
-  return bcrypt.compare(inputPassword, this.password)
-}
+userSchema.pre("save", function (next) {
+  const user = this;
 
+  if (!user.isModified("password")) return next(); // only run if password changed
 
-module.exports = function() {
+  bcrypt
+    .genSalt()
+    .then((salt) => {
+      return bcrypt.hash(user.password, salt);
+    })
+    .then((hash) => {
+      user.password = hash;
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+userSchema.methods.comparePassword = function (inputPassword) {
+  return bcrypt.compare(inputPassword, this.password);
+};
+
+module.exports = function () {
   logger.info(`Connecting to ${process.env.MONGO_URL}`);
   const conn = mongoose.createConnection(process.env.MONGO_URL);
-  conn.model('User', userSchema);
-  
+  conn.model("User", userSchema);
+
   return conn;
-}
+};
