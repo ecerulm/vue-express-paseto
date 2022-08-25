77dddb0f01605f43377368f88e036376f60b2dba
Ruben Laguna <ruben.laguna@gmail.com>
2022-08-25 22:11:07 +0200

    frontend: Add login button to navbar

--- frontend-vue/src/components/NavBar.vue
+++ frontend-vue/src/components/NavBar.vue
@@ -1,19 +1,8 @@
-
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-      </ul>
-
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button type="button" class="btn btn-outline-primary">Login</button>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
